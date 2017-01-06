8c33bf96b124228e1d8e852254723b6221f1c7fe
Natalia Rakovska <natka.rak@gmail.com>
2017-01-06 13:02:29 +0100

    had to sart over...

--- django/GfLib/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-#from django.contrib import admin
-
-from . import views
-
-urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^logout/', views.logout_user, name='logout'),
-]
