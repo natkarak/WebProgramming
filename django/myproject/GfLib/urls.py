491044a43f88d486f1c1aa2eb318c89b69d51db2
Natalia Rakovska <natka.rak@gmail.com>
2017-01-15 04:53:20 +0100

    docu

--- django/myproject/GfLib/urls.py
+++ django/myproject/GfLib/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
 	url(r'^dashboard/', views.dashboard, name='dashboard'),
 	url(r'^register/', views.register, name='register'),
 	url(r'^search/', views.search, name='search'),
-#	url(r'^searchResult/', views.searchResult, name='searchResult'),
+	url(r'^searchResult/', views.searchResult, name='searchResult'),
 	url(r'^logout/', views.logout_view, name='logout'),
 	url(r'^login/$', views.login_view, name = 'login'),
+	url(r'^([0-9]+)/$', views.detail, name = 'detail'),
 ]
